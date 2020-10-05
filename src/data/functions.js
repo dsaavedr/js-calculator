254c4d03b718eb4580b8e275e8794b6312e0f305
Daniel Saavedra <danielsaavedram@hotmail.com>
2020-10-05 17:18:00 -0500

    Moved data to separate folder

---
+++ src/data/functions.js
@@ -0,0 +1,30 @@
+export default [
+    {
+        id: "clear",
+        symbol: "AC"
+    },
+    {
+        id: "divide",
+        symbol: "/"
+    },
+    {
+        id: "multiply",
+        symbol: "X"
+    },
+    {
+        id: "subtract",
+        symbol: "-"
+    },
+    {
+        id: "add",
+        symbol: "+"
+    },
+    {
+        id: "equals",
+        symbol: "="
+    },
+    {
+        id: "decimal",
+        symbol: "."
+    }
+];
