97d31046c1d234993124b7c96ea9f43114635333
Daniel Saavedra <danielsaavedram@hotmail.com>
2020-10-12 21:34:42 -0500

    Production version of js-calculator for Github-pages

---
+++ build/precache-manifest.b9485dd4ceb99bc801426014ba21d51d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fccf72ec5760733ddee3f3fd539058aa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4a9ea68b450ea8cb8a64",
+    "url": "/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "05d650177f2e9fecd674",
+    "url": "/static/css/main.82f42790.chunk.css"
+  },
+  {
+    "revision": "4a9ea68b450ea8cb8a64",
+    "url": "/static/js/2.7bbe4bd5.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.7bbe4bd5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "05d650177f2e9fecd674",
+    "url": "/static/js/main.03f8ad2d.chunk.js"
+  },
+  {
+    "revision": "3964ef07048d76e304d1",
+    "url": "/static/js/runtime-main.7978786b.js"
+  }
+]);
